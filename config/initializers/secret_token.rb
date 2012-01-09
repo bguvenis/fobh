938c045e6e24520914d17096b672e8bbe74b25d7
Baris Guvenis <bguvenis@gmail.com>
2012-01-09 21:46:25 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fobh::Application.config.secret_token = '4742eb5523cc664818f4f6f36320618e6cdf1430d3f9cc07c38b842eb87bf6b10e20ca64bbb6e952de00698826bd481ab389f8c1195f23a0a46d05413ec233ba'
